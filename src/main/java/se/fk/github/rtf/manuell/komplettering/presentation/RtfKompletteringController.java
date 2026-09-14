9fac6bdaf313330b747ed3df3d163a9157dd60ef
Nils Elveros <nils.elveros@tromb.com>
2026-09-14 15:02:34 +0200

    fix: added controller for komplettering

---
+++ src/main/java/se/fk/github/rtf/manuell/komplettering/presentation/RtfKompletteringController.java
@@ -0,0 +1,11 @@
+package se.fk.github.rtf.manuell.komplettering.presentation;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import se.fk.rimfrost.framework.regel.komplettering.presentation.rest.RegelKompletteringController;
+import se.fk.rimfrost.regel.rtf.manuell.jaxrsspec.controllers.generatedsource.model.RtfKompletteringData;
+
+@ApplicationScoped
+public class RtfKompletteringController extends RegelKompletteringController<RtfKompletteringData>
+{
+
+}
