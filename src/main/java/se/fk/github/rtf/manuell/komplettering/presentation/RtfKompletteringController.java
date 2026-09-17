2b39fe2f58619bca11cbc1b6a8c93f7ff48549a7
Ulf Slunga <ulf.slunga@sinetiq.se>
2026-09-17 14:13:42 +0200

    fix: replace rimfrost-regel-rtf-manuell-openapi with rimfrost-regel-rtf-manuell-komplettering-openapi

--- src/main/java/se/fk/github/rtf/manuell/komplettering/presentation/RtfKompletteringController.java
+++ src/main/java/se/fk/github/rtf/manuell/komplettering/presentation/RtfKompletteringController.java
@@ -3,7 +3,7 @@ package se.fk.github.rtf.manuell.komplettering.presentation;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.Path;
 import se.fk.rimfrost.framework.regel.komplettering.presentation.rest.RegelKompletteringController;
-import se.fk.rimfrost.regel.rtf.manuell.jaxrsspec.controllers.generatedsource.model.RtfKompletteringData;
+import se.fk.rimfrost.regel.rtf.manuell.komplettering.jaxrsspec.controllers.generatedsource.model.RtfKompletteringData;
 
 @Path("/regel/rtf-manuell-komplettering")
 @ApplicationScoped
